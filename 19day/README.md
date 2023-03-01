748151abc26ac501fa923548a7ecd04ffa3fa1c9
talhaulutas <talha.ulutas12@gmail.com>
2023-03-01 13:29:54 +0300

    firstcmt

--- 19day/README.md
+++ 19day/README.md
@@ -1,4 +1,4 @@
-# Day18
+# 19day
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.1.6.
 
--- 19day/src/app/app.component.spec.ts
+++ 19day/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'day18'`, () => {
+  it(`should have as title '19day'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('day18');
+    expect(app.title).toEqual('19day');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('day18 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('19day app is running!');
   });
 });
