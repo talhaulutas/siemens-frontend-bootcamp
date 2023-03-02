a5c4359b9afbd1a88abe7ea1ebe14a24a4fd5ad0
talhaulutas <talha.ulutas12@gmail.com>
2023-03-02 13:32:14 +0300

    temizlik

--- inclass/src/app/app-routing.module.ts
+++ inclass/src/app/app-routing.module.ts
@@ -1,26 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
-import { ProductListComponent } from './admin/product-list/product-list.component';
-import { CanActivateChildFn, canActivateNewFormat, CanOrderLoad, EditGuardNewType } from './guards';
-import { HomeComponent } from './main/home/home.component';
-import { LoginComponent } from './main/login/login.component';
 
-const routes: Routes = [
-  {path:'',component:HomeComponent },
-  {path:'home',component:HomeComponent },
-  {path:'login',component:LoginComponent },
-  {
-    path:"admin",canActivate:[canActivateNewFormat],canActivateChild:[CanActivateChildFn],children:[
-      { path: '', component: AdminHomeComponent },
-      { path: 'home', component: AdminHomeComponent },
-      { path: 'product-list', component: ProductListComponent,canDeactivate: [EditGuardNewType]},
-    ]
-  },
-  {
-    path:"lazy-order",canLoad:[CanOrderLoad],loadChildren:()=> import('./order/order.module').then(m=>m.OrderModule)
-  }
-];
+
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- inclass/src/app/app.module.ts
+++ inclass/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AdminModule } from './admin/admin.module';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +12,6 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
