c90a10595ff7028bca308c6cceab2d43a561d39d
talhaulutas <talha.ulutas12@gmail.com>
2023-03-02 11:45:54 +0300

    temizlik

--- app/src/app/canLoad.ts
+++
@@ -1,5 +0,0 @@
-import { Route, UrlSegment } from "@angular/router";
-
-export const CanOrderLoad = (route:Route,segments:UrlSegment[])=>{
-    return true;
-  }
--- inclass/src/app/app.module.ts
+++ inclass/src/app/app.module.ts
@@ -1,21 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AdminModule } from './admin/admin.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-
 @NgModule({
   declarations: [
-    AppComponent,
-    
-    
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    AdminModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
