df9bb4bfc795f266bea0a3513cceacdd0b8d111d
talhaulutas <talha.ulutas12@gmail.com>
2023-03-04 12:04:42 +0300

    frstcmt

--- hw/src/app/app.module.ts
+++ hw/src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
